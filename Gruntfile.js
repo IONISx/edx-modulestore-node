d75ec99636b6a9d7851a59a91398d3a479d6d41f
stv <stv@stanford.edu>
2014-11-18 15:23:48 -0500

    Move jshint options to .jshintrc
    JSHint's configuration has been moved _out_ of the `Gruntfile.js` and
    _into_ an external `.jshintrc`.
    
    Note: No new options have been added; only previously existing options
    have been migrated. One exception is the `trailing` option which was
    excluded, as JSHint does not appear to currently support this option [1].
    
    [1] http://www.jshint.com/docs/options/

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,22 +6,7 @@ module.exports = function (grunt) {
     grunt.initConfig({
         jshint: {
             options: {
-                'node': true,
-                'curly': true,
-                'eqeqeq': true,
-                'immed': true,
-                'latedef': true,
-                'newcap': true,
-                'noarg': true,
-                'noempty': true,
-                'unused': true,
-                'undef': true,
-                'trailing': true,
-                'indent': 2,
-                'quotmark': 'single',
-                'boss': true,
-                'eqnull': true,
-                'strict': true
+                jshintrc: true,
             },
             node: {
                 files: {
